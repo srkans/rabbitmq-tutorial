914cb46493e62fd8709898ffd55326f749a612ed
srkans <serkansacma@hotmail.com>
2023-08-01 14:01:58 +0300

    basic producer

---
+++ first-app/consumer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory{HostName="localhost"};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue:"letterbox",durable:false,exclusive:false,autoDelete:false,arguments:null);
+
+var consumer = new EventingBasicConsumer(channel);
+
---
+++ first-app/producer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{HostName="localhost"};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue:"letterbox",durable:false,exclusive:false,autoDelete:false,arguments:null);
+
+var message = "This is my first message";
+
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+//we always have to publish to an exchange
+channel.BasicPublish("","letterbox", null, encodedMessage);
+
+Console.WriteLine($"Published message : {message}");
