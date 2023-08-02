d4049758edfb0f8da448408c082c66903ffde904
srkans <serkansacma@hotmail.com>
2023-08-02 14:16:26 +0300

    pubsub design

---
+++ pubsub-design/consumer/consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+string exchangeName = "sample-pubsub-example";
+
+channel.ExchangeDeclare(exchange: exchangeName,
+                        type: ExchangeType.Fanout);
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(queue:queueName,
+                  exchange:exchangeName,
+                  routingKey:string.Empty);
+
+channel.BasicQos(prefetchCount:1,
+                 prefetchSize:0,
+                 global:false);
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(queue:queueName,
+                     autoAck:false,
+                     consumer:consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Console.WriteLine($"Message : {message}");
+};
+
+Console.Read();
---
+++ pubsub-design/producer/producer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+string exchangeName = "sample-pubsub-example";
+
+channel.ExchangeDeclare(exchange:exchangeName,
+                        type:ExchangeType.Fanout);
+
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+
+channel.BasicPublish(exchange: exchangeName,
+                    routingKey:string.Empty,
+                    body:message);
+
+Console.Read();
