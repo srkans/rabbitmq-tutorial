ed87996732e8d947e49d0a6a6796946ef7beee2f
srkans <serkansacma@hotmail.com>
2023-08-02 09:14:51 +0300

    direct exchange behaviour

---
+++ direct-exchange/consumer/consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "sample-direct-exchange", type: ExchangeType.Direct);
+
+string queueName = channel.QueueDeclare(exclusive:false).QueueName;
+
+channel.QueueBind(queue:queueName, 
+                 exchange:"sample-direct-exchange",
+                 routingKey:"sample-direct-queue");
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer:consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine($"Consume edildi : {message}");
+};
+
+Console.Read();
---
+++ direct-exchange/producer/producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "sample-direct-exchange", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Mesaj : ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange:"sample-direct-exchange",
+                         routingKey:"sample-direct-queue", 
+                         body:byteMessage);
+}
+
+Console.Read();
