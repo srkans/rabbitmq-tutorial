1373949e74e381b974c73de3bb688fe2e6090b26
srkans <serkansacma@hotmail.com>
2023-08-02 09:49:21 +0300

    fanout exchange

---
+++ fanout-exchange/consumer/consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "sample-fanout-exchange", type: ExchangeType.Fanout);
+
+Console.Write("Enter queue name : ");
+
+string queueName = Console.ReadLine();
+
+channel.QueueDeclare(queue:queueName,exclusive:false);
+
+channel.QueueBind(queue: queueName, 
+                  exchange: "sample-fanout-exchange",
+                  routingKey:string.Empty);
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(queue: queueName,
+                     autoAck:true,
+                     consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+
+    Console.WriteLine($"Mesaj : {message}");
+};
+
+Console.Read();
---
+++ fanout-exchange/producer/producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "sample-fanout-exchange", type: ExchangeType.Fanout);
+
+for(int i = 0; i < 100; i++)
+{
+    await Task.Delay(100);
+
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba : {i}");
+
+    channel.BasicPublish(exchange: "sample-fanout-exchange",
+                         routingKey: string.Empty,
+                         body:message);
+}
+
+Console.Read();
