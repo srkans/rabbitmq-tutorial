a19e5526fdfa8208b4350a5e2bd36a1a2cbf3f2d
srkans <serkansacma@hotmail.com>
2023-08-02 12:13:31 +0300

    headers exchange

---
+++ header-exchange/consumer/consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "sample-header-exchange",
+                        type: ExchangeType.Headers);
+
+Console.Write("Header value'yu giriniz : ");
+string value = Console.ReadLine();
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(queue: queueName,
+                  exchange: "sample-header-exchange",
+                  routingKey: string.Empty,
+                  new Dictionary<string, object>
+                  {
+                      ["no"] = value
+                  });
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(queue: queueName,
+                    autoAck:true,
+                    consumer:consumer
+                    );
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ header-exchange/producer/producer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "sample-header-exchange",
+                        type:ExchangeType.Headers);
+
+for(int i = 0; i < 10; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba : {i}");
+
+    Console.Write("Header value giriniz : ");
+    string value = Console.ReadLine();
+
+    IBasicProperties basicProperties = channel.CreateBasicProperties();
+    basicProperties.Headers = new Dictionary<string, object>
+    {
+        ["no"] = value
+    };
+
+    channel.BasicPublish(exchange: "sample-header-exchange",
+                        routingKey:string.Empty,
+                        body:message,
+                        basicProperties:basicProperties);
+}
+
+Console.Read();
