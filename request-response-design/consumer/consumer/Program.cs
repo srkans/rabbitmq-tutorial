2170ee8a77c3c1264cdec2e4f6d7716be94dec2d
srkans <serkansacma@hotmail.com>
2023-08-02 20:07:45 +0300

    request response design

---
+++ request-response-design/consumer/consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+string requestQueueName = "sample-request-queue";
+
+channel.QueueDeclare(queue: requestQueueName,
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false);
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(queue:requestQueueName,
+                     autoAck:true,
+                     consumer:consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+
+    Console.WriteLine(message);
+
+    byte[] responseMessage = Encoding.UTF8.GetBytes($"Islem tamamlandi : {message}");
+
+    IBasicProperties basicProperties = channel.CreateBasicProperties();
+
+    basicProperties.CorrelationId = e.BasicProperties.CorrelationId;
+
+    channel.BasicPublish(exchange:string.Empty,
+                         routingKey:e.BasicProperties.ReplyTo,
+                         basicProperties:basicProperties,
+                         body:responseMessage);
+
+};
+
+Console.Read();
---
+++ request-response-design/producer/producer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+string requestQueueName = "sample-request-queue";
+
+channel.QueueDeclare(queue:requestQueueName,
+                     durable:false,
+                     exclusive:false,
+                     autoDelete:false);
+
+string responseQueueName = channel.QueueDeclare().QueueName;
+
+string correlationId = Guid.NewGuid().ToString();
+
+//request message
+IBasicProperties basicProperties = channel.CreateBasicProperties();
+
+basicProperties.CorrelationId = correlationId;
+
+basicProperties.ReplyTo = responseQueueName;
+
+for(int i = 0; i < 15; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+
+    channel.BasicPublish(exchange:string.Empty,
+                         routingKey:requestQueueName,
+                         body:message,
+                         basicProperties:basicProperties);
+}
+
+//response message
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(queue:responseQueueName,
+                     autoAck:true,
+                     consumer:consumer);
+
+consumer.Received += (sender, e) =>
+{
+    if(e.BasicProperties.CorrelationId == correlationId)
+    {
+        string message = Encoding.UTF8.GetString(e.Body.Span);
+
+        Console.WriteLine($"Rsponse : {message}");
+    }
+};
+
+Console.Read();
