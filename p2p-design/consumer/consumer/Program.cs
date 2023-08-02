a8bba0efd5d07f5863f7a1fc79c8b838bb68723f
srkans <serkansacma@hotmail.com>
2023-08-02 14:02:08 +0300

    p2p design

---
+++ p2p-design/consumer/consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+string queueName = "sample-p2p-queue";
+
+channel.QueueDeclare(queue: queueName,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false);
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(consumer: consumer,
+                     queue : queueName,
+                     autoAck:false);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine($"Message : {message}");
+};
+
+Console.Read();
---
+++ p2p-design/producer/producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+string queueName = "sample-p2p-queue";
+
+channel.QueueDeclare(queue: queueName,
+                    durable:false,
+                    exclusive:false,
+                    autoDelete:false);
+
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+
+channel.BasicPublish(exchange: string.Empty,
+                     routingKey: queueName,
+                     body:message);
+
+Console.Read();
