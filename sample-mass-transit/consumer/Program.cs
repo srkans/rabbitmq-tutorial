ebf4bf29f7f17ef67334fda9b925fad1b1a7ace7
srkans <serkansacma@hotmail.com>
2023-08-02 21:34:09 +0300

    mass transit sample

---
+++ sample-mass-transit/consumer/Program.cs
@@ -0,0 +1,19 @@
+﻿using consumer.Consumers;
+using MassTransit;
+
+string queueName = "sample-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(host: "amqps://localhost:5672");
+
+    factory.ReceiveEndpoint(queueName, endpoint =>
+    {
+        endpoint.Consumer<SampleMessageConsumer>();
+    });
+});
+
+await bus.StartAsync();
+
+Console.Read();
+
---
+++ sample-mass-transit/producer/Program.cs
@@ -0,0 +1,22 @@
+﻿using MassTransit;
+using shared.Messages;
+
+string queueName = "sample-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(host: "amqps://localhost:5672");
+});
+
+ISendEndpoint sendEndpoint = await bus.GetSendEndpoint(new($"amqps://localhost:15672/{queueName}"));
+
+Console.Write("Gonderilecek mesaj : ");
+string message = Console.ReadLine();
+
+await sendEndpoint.Send<IMessage>(new SampleMessage()
+{
+    Text = message
+});
+
+Console.Read();
+
