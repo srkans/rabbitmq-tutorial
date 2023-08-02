325bc42631a8d59216c818de3bd0e2400acdda53
srkans <serkansacma@hotmail.com>
2023-08-02 14:43:02 +0300

    work queue design

---
+++ work-queue-design/consumer/consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+string queueName = "sample-work-queue";
+
+channel.QueueDeclare(queue: queueName,
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false);
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(queue:queueName,
+                     autoAck:true,
+                     consumer:consumer);
+
+channel.BasicQos(prefetchCount:1,
+                 prefetchSize:0,
+                 global:false);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ work-queue-design/producer/producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+string queueName = "sample-work-queue";
+
+channel.QueueDeclare(queue:queueName,
+                     durable:false,
+                     exclusive:false,
+                     autoDelete:false);
+
+for(int  i = 0; i < 50; i++)
+{
+    await Task.Delay(200);
+
+    byte[] message = Encoding.UTF8.GetBytes($"Selam {i}");
+
+    channel.BasicPublish(exchange:string.Empty,
+                         routingKey:queueName,
+                         body:message);
+}
+
+Console.Read();
